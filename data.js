12dd7b5c625e83cbc078ccd88487aaeade95e26a
Chanakarn <88476014+64070018@users.noreply.github.com>
2023-02-28 16:23:17 +0700

    add data

---
+++ data.js
@@ -0,0 +1,21 @@
+const rooms = JSON.parse(`[{
+    "type": "Standard Room",
+    "price": 999,
+    "description": "ห้องราคาดี มีเฟอร์นิเจอร์พร้อม เป็นมิตรกับทุกคน",
+    "img": ""
+  }, {
+    "type": "Deluxe Room",
+    "price": 1499,
+    "description": "ห้องราคาดี มีเฟอร์นิเจอร์พร้อม เป็นมิตรกับทุกคน",
+    "img": ""
+  }, {
+    "type": "Suite Room",
+    "price": 1999,
+    "description": "ห้องราคาดี มีเฟอร์นิเจอร์พร้อม เป็นมิตรกับทุกคน",
+    "img": ""
+  }, {
+    "type": "Superior Room",
+    "price": 2499,
+    "description": "ห้องราคาดี มีเฟอร์นิเจอร์พร้อม เป็นมิตรกับทุกคน",
+    "img": ""
+  }]`);
