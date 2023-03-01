4e1491beffea8607f89091d683588bdc065c6699
PUTTIPONG3035 <64070078@kmitl.ac.th>
2023-03-02 00:41:46 +0700

    folk

--- data/limits.js
+++ data/limits.js
@@ -1,18 +1,17 @@
-
-const limits = JSON.parse(`[{
+const data_range = JSON.parse(`[{
     "id": "01",
     "type": "Standard Room",
-    "range": "0 - 999",
+    "range": "0 - 999"
   }, {
     "id": "02",
     "type": "Deluxe Room",
-    "range": "1000 - 1499",
+    "range": "1000 - 1499"
   }, {
     "id": "03",
     "type": "Suite Room",
-    "range": "1500 - 1999",
+    "range": "1500 - 1999"
   }, {
     "id": "04",
     "type": "Superior Room",
-    "range": "2000 - 2499",
+    "range": "2000 - 2499"
   }]`);
