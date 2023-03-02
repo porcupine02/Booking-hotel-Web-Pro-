aa20cc149619ba15c9b943b682d4f80030ea8aec
PUTTIPONG3035 <64070078@kmitl.ac.th>
2023-03-02 21:16:44 +0700

    profile

---
+++ data/profile.js
@@ -0,0 +1,10 @@
+const profiles = JSON.parse(`[{
+    "id": "01",
+    "user_name" : "Web Pro",
+    "first_name": "Web",
+    "last_name": "Pro",
+    "email": "webPro.gmail.com",
+    "phone": "084-9999-1234",
+    "birth_date" : "24-01-2002",
+    "img": "./asset/standard.jpg"
+  }]`);
