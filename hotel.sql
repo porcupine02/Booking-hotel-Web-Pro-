54bf48a328aef66495d83bfcc4a96861858cd6e5
PUTTIPONG3035 <88504764+PUTTIPONG3035@users.noreply.github.com>
2023-03-25 21:35:06 +0700

    Update hotel.sql
    sql new แต่ยังไม่สมบูรณ์

--- hotel.sql
+++ hotel.sql
@@ -13,7 +13,7 @@ CREATE TABLE `customers` (
 DROP TABLE IF EXISTS `logIn`;
 CREATE TABLE `logIn` (
   `login_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
-  `cus_id` int(5)  NOT NULL,
+  `cus_id` int(5) unsigned NOT NULL,
   `user_name` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL,
   `login_role` enum('manager', 'customer') NOT NULL DEFAULT 'customer',
@@ -22,18 +22,31 @@ CREATE TABLE `logIn` (
    
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
+DROP TABLE IF EXISTS `roomDetail`;
+CREATE TABLE `roomDetail` (
+  `room_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `room_img_id` int(2) unsigned NOT NULL,
+  `room_type` varchar(25)  NOT NULL,
+  `price` int(10)  NOT NULL,
+  `rate` int(1) ,
+  `description` varchar(500),
+  `service_id` int(5) unsigned,
+  PRIMARY KEY (`room_id`)
+
+   
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `booking`;
 CREATE TABLE `booking` (
   `booking_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
-  `cus_id` int(5) NOT NULL,  
-  `room_id` int(5) NOT NULL,
+  `cus_id` int(5) unsigned NOT NULL,  
+  `room_id` int(5) unsigned NOT NULL,
   `check_in` date NOT NULL,
   `check_out` date NOT NULL,
   `Promotion_code` varchar(5),
   `booking_fname` varchar(25) NOT NULL,
   `booking_lname` varchar(25)  NOT NULL,
-  `timestamp_booking` datetime  NOT NULL AUTO_INCREMENT,
+  `timestamp_booking` datetime  NOT NULL,
   `people` int(1) NOT NULL,
   `countRoom` int(2) NOT NULL,
   PRIMARY KEY (`booking_id`),
@@ -42,32 +55,16 @@ CREATE TABLE `booking` (
    
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
-
-DROP TABLE IF EXISTS `roomDetail`;
-CREATE TABLE `roomDetail` (
-  `room_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
-  `room_img_id` int(2) NOT NULL,
-  `room_type` varchar(25)  NOT NULL,
-  `price` int(10)  NOT NULL,
-  `rate` int(1) ,
-  `description` varchar(500),
-  `service_id` int(5),
-  PRIMARY KEY (`room_id`),
-  FOREIGN KEY (`room_img_id`) REFERENCES `image`(`room_img_id`),
-  FOREIGN KEY (`service_id`) REFERENCES `service`(`service_id`)
-   
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-
-
 DROP TABLE IF EXISTS `vacantRoom`;
 CREATE TABLE `vacantRoom` (
-  `room_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `room_id`int(5) unsigned NOT NULL AUTO_INCREMENT,
   `count` int(2)  NOT NULL,
   `date` date,
-  PRIMARY KEY (`login_id`, `room_id`),
-  FOREIGN KEY (`room_id`) REFERENCES `roomDeatil`(`room_id`)
+  PRIMARY KEY (`date`, `room_id`),
+  FOREIGN KEY (`room_id`) REFERENCES `roomDetail`(`room_id`)
    
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
 
 
 DROP TABLE IF EXISTS `payment`;
@@ -76,7 +73,7 @@ CREATE TABLE `payment` (
   `payment_date` date  NOT NULL,
   `payment_slip` varchar(40) NOT NULL,
   `payment_total_money` int(10) NOT NULL,
-  `booking_id` int(5) NOT NULL ,
+  `booking_id` int(5) unsigned NOT NULL ,
   PRIMARY KEY (`payment_id`),
   FOREIGN KEY (`booking_id`) REFERENCES `booking`(`booking_id`)
    
