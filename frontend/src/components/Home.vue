b25c4908645a270981722bfaa028b9345be4199b
PUTTIPONG3035 <64070078@kmitl.ac.th>
2023-04-05 07:29:55 +0700

    src = '/standard1.jpg'

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="home">
+      <div id="home">
         <!-- login signout -->
         <div :class="{modal: modal, 'is-active': isActive_Sign_in}">
             <div class="modal-background"></div>
             <form class="box  modal-content">
                 <div class="field">
-                    <label class="label has-text-centered">Sign inn</label>
+                    <label class="label has-text-centered">Sign in</label>
                 </div>
                 <div class="field">
                     <h1 class="label">Email</h1>
@@ -85,6 +85,61 @@
                 <button class="button is-danger" @click="Close()">Close</button>
             </form>
         </div>
+
+        <!-- nav bar -->
+        <nav class="navbar is-dark" role="navigation" aria-label="main navigation"
+            style="position:fixed; top: 0; width: 100%;">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="http://localhost:8080/">
+                    <i class="fas fa-home "></i>
+                  Home
+                </a>
+            </div>
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" v-if="logins == true" href="search.html">
+                        ค้นหาห้องพัก
+                    </a>
+
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            ห้องพักของเรา
+                        </a>
+
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item" href="index.html#01">
+                                Standard Room
+                            </a>
+                            <a class="navbar-item" href="index.html#02">
+                                Deluxe Room
+                            </a>
+                            <a class="navbar-item" href="index.html#03">
+                                Suite Room
+                            </a>
+                            <a class="navbar-item" href="index.html#04">
+                                Superior Room
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons asd">
+                            <a class="button is-info" v-if="logins == false" @click="isActive_Sign_up = true">
+                                <strong>Sign up</strong>
+                            </a>
+                            <a class="button is-dark" v-if="logins == true" href="profile.html">
+                                <strong>My Profile</strong>
+                            </a>
+                            <a class="button is-dark" v-if="logins == false" @click="isActive_Sign_in = true">
+                                Log in
+                            </a>
+                            <a class="button is-danger" v-if="logins == true" @click="logins = false">Log out</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
         <!-- banner -->
 
         <section class="hero has-background-info-light is-large bannerImage">
@@ -99,25 +154,22 @@
         </section>
 
 
-
-
+   
+        
         <!-- content -->
-        <div class="container my-5" v-for="room in rooms" :key="room">
-
+        <div class="container my-5" v-for="room in rooms">
+            
             <div id="room.room_id">
                 <div class="tile is-ancestor my-5 has-background-danger-light">
                     <div class="tile is-4 is-vertical is-parent">
                         <div class="tile is-child">
-                         
-                            <img :src="room.pic1" alt="image_room" style="width: 100%; height: 85%;">
-                            <h1>   {{ room.pic1 }}</h1>
-                            <!-- ../assets/standard1.jpg -->
+                            
+                            <img :src="room.pic1" alt="" style="width: 100%;">
                             <fa icon="star" class=" mt-3" style="font-size:40px ;color:rgb(244, 247, 76)"></fa>
                             <fa icon="star" class=" mt-3" style="font-size:40px ;color:rgb(244, 247, 76)"></fa>
                             <fa icon="star" class=" mt-3" style="font-size:40px ;color:rgb(244, 247, 76)"></fa>
                             <fa icon="star" class=" mt-3" style="font-size:40px ;color:rgb(188, 188, 165)"></fa>
                             <fa icon="star" class=" mt-3" style="font-size:40px ;color:rgb(188, 188, 165)"></fa>
-
                         </div>
                     </div>
                     <div class="tile is-parent is-vertical">
@@ -129,16 +181,16 @@
                         <hr>
                         <p class="subtitle is-5">Facilities</p>
                         <div class="columns">
-                            <div class="column is-2 has-text-centered" v-if="room.breakfast == 'yes'">
+                            <div class="column is-2 has-text-centered"  v-if="room.breakfast == 'yes'">
                                 <fa icon="coffee" style="font-size:50px"></fa><br>
                                 breakfast
                             </div>
 
-                            <div class="column is-2 has-text-centered" v-if="room.wifi == 'yes'">
+                            <div class="column is-2 has-text-centered"  v-if="room.wifi == 'yes'">
                                 <fa icon="wifi" style="font-size:50px"></fa><br>
                                 internet
                             </div>
-                            <div class="column is-2 has-text-centered" v-if="room.air_conditioner == 'yes'">
+                            <div class="column is-2 has-text-centered"  v-if="room.air_conditioner == 'yes'">
                                 <fa icon="wifi" style="font-size:50px"></fa><br>
                                 internet
                             </div>
@@ -159,6 +211,31 @@
             </div>
         </div>
 
+        <!-- Contact -->
+        <div class="container">
+            <div class="columns">
+                <div class="column is-7">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15513.042363639155!2d100.653445!3d13.580884599999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d5fb6535ef921%3A0x92a8402c649e96b8!2z4LiV4Lil4Liy4LiU4LmA4LiL4LmI4LmA4Lin4LmI4LiZ!5e0!3m2!1sth!2sth!4v1679677449385!5m2!1sth!2sth"
+                        width="100%" height="500" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="column is-5">
+                    <p class="title is-5 has-text-centered">
+                        Contact Me
+                    </p>
+                    <p class="subtitle is-6">
+                        Tell : <br>
+                        Facebook : <br>
+                        Instargram : <br>
+                        E-mail : <br>
+                    </p>
+                </div>
+            </div>
+
+
+
+        </div>
     </div>
 </template>
 
@@ -168,11 +245,14 @@ import axios from "axios";
 /* add fontawesome core */
 
 
+
 export default {
 
   data() {
     return {
       rooms: [],
+      filename : '~',
+      link: 'standard1.jpg',
       modal: true,
       isActive_Sign_in: false,
       isActive_Sign_up: false,
@@ -184,7 +264,6 @@ export default {
       sign_email: '',
       sign_username: '',
       sign_pass: '',
-      image: 'https://hips.hearstapps.com/hmg-prod/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=1.00xw:0.756xh;0,0.0756xh&resize=1200:*'
     };
   },
   created() {
@@ -209,8 +288,17 @@ export default {
     login() {
         this.logins = true;
         this.isActive_Sign_in = false
-    }
+    },
+  
+    
   },
+
+//   computed:{
+//      images(){
+//          return require(`../assets/standard1.jpg`)
+
+//     }
+//   }
 };
 </script>
 
